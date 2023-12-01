26a0611030e84b82545320a2f97845b3b521d19d
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-02 02:24:12 +0300

    MongoDB Connection String and Environment Variables

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
+
 
 //db connect
 dbConnect();
 const app = express();
-
-
-
 
 export default app;
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const dbConnect = async () => {
   try {
     mongoose.set('strictQuery', false);
-    const connected = await mongoose.connect('url');
+    const connected = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb connected ${connected.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
