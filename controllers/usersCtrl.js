0619e97399a10d5963cf0cb261675297b1a8522a
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-04 06:41:00 +0300

    User Registration Controller

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -5,7 +5,25 @@ import User from '../model/User.js';
 // @access  Private/Admin
 
 export const registerUserCtrl = async (req, res) => {
-  res.json({
-    msg: 'User register controller',
+  const { fullname, email, password } = req.body;
+  //Check user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    //throw
+    res.json({
+      msg: 'User already exists',
+    });
+  }
+  //hash password
+  //create the user
+  const user = await User.create({
+    fullname,
+    email,
+    password,
+  });
+  res.status(201).json({
+    status: 'success',
+    message: 'User Registered Successfully',
+    data: user,
   });
 };
