a632b9224a83abd221f6d6a566545fb806c2f542
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-05 05:14:18 +0300

    Hash User Password

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,4 +1,5 @@
 import User from '../model/User.js';
+import bcrypt from 'bcryptjs';
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
@@ -15,11 +16,13 @@ export const registerUserCtrl = async (req, res) => {
     });
   }
   //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
   //create the user
   const user = await User.create({
     fullname,
     email,
-    password,
+    password: hashedPassword,
   });
   res.status(201).json({
     status: 'success',
