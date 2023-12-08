0c63868e3dd6fd5569ae6173a53ab83b572fafc7
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-08 12:00:53 +0300

    Not Found Route Handler

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/usersRoute.js';
-import { globalErrhandler } from '../middlewares/globalErrHandler.js';
+import { globalErrhandler, notFound } from '../middlewares/globalErrHandler.js';
 
 //db connect
 dbConnect();
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/', userRoutes);
 
 //err middleware
+app.use(notFound);
 app.use(globalErrhandler);
 
 export default app;
