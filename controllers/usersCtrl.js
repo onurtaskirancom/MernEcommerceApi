7ee471bd83ba54b870e3bace521edc017e0e0393
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-09 11:58:59 +0300

    Generate Token For Login User

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,6 +1,7 @@
 import User from '../model/User.js';
 import asyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Register user
 // @route   POST /api/v1/users/register
@@ -45,6 +46,7 @@ export const loginUserCtrl = asyncHandler(async (req, res) => {
       status: 'success',
       message: 'User logged in successfully',
       userFound,
+      token: generateToken(userFound?._id),
     });
   } else {
     throw new Error('Invalid login credentials');
