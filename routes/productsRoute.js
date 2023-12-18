d6081f375f068452575389d9b6266ea62d7eeefc
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-18 16:53:05 +0300

    Fetching All Products

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,13 @@
 import exppress from 'express';
-import { createProductCtrl } from '../controllers/productsCtrl.js';
+import {
+  createProductCtrl,
+  getProductsCtrl,
+} from '../controllers/productsCtrl.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
 const productsRouter = exppress.Router();
 
 productsRouter.post('/', isLoggedIn, createProductCtrl);
+productsRouter.get('/', getProductsCtrl);
 
 export default productsRouter;
