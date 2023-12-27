47c9045e6d2653254a564aeaa24aad917968b086
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-28 01:27:57 +0300

    Single, Update, Delete Categories Controller

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,11 +1,20 @@
 import exppress from 'express';
 
-import { createCategoryCtrl } from '../controllers/categoriesCtrl.js';
+import {
+  createCategoryCtrl,
+  getAllCategoriesCtrl,
+  getSingleCategoryCtrl,
+  updateCategoryCtrl,
+  deleteCategoryCtrl,
+} from '../controllers/categoriesCtrl.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
 const categoriesRouter = exppress.Router();
 
 categoriesRouter.post('/', isLoggedIn, createCategoryCtrl);
-
+categoriesRouter.get('/', getAllCategoriesCtrl);
+categoriesRouter.get('/:id', getSingleCategoryCtrl);
+categoriesRouter.delete('/:id', deleteCategoryCtrl);
+categoriesRouter.put('/:id', updateCategoryCtrl); 
 
 export default categoriesRouter;
