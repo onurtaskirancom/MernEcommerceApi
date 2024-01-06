d292d485cffc9c7e6775bcdd35a2143525267df2
onurtaskirancom <taskiranonur9@gmail.com>
2024-01-06 18:54:43 +0300

    Calculate Average Rating

--- model/Product.js
+++ model/Product.js
@@ -71,7 +71,25 @@ const ProductSchema = new Schema(
     toJSON: { virtuals: true },
   }
 );
-
+//Virtuals
+//Total rating
+ProductSchema.virtual('totalReviews').get(function () {
+  const product = this;
+  return product?.reviews?.length;
+});
+//average Rating
+ProductSchema.virtual('averageRating').get(function () {
+  let ratingsTotal = 0;
+  const product = this;
+  product?.reviews?.forEach((review) => {
+    ratingsTotal += review?.rating;
+  });
+  //calc average rating
+  const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(
+    1
+  );
+  return averageRating;
+});
 const Product = mongoose.model('Product', ProductSchema);
 
 export default Product;
