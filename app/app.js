22a9fce95194c5fea5640574972871e0274080eb
onurtaskirancom <taskiranonur9@gmail.com>
2024-01-26 05:49:36 +0300

    WebHook Error Fixed

--- app/app.js
+++ app/app.js
@@ -16,6 +16,50 @@ import brandsRouter from '../routes/brandsRouter.js';
 dbConnect();
 const app = express();
 
+
+
+//Stripe webhook
+//stripe instance
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  'whsec_bcb4cd56075994cfc386e015d9fcd8e511e5f5177aacb9ab18a1c3fac59effba';
+
+app.post(
+  '/webhook',
+  express.raw({ type: 'application/json' }),
+  (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      console.log('event');
+    } catch (err) {
+      console.log('err', err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+
 //pass incoming data
 app.use(express.json());
 
@@ -27,43 +71,6 @@ app.use('/api/v1/brands/', brandsRouter);
 app.use('/api/v1/colors/', colorRouter);
 app.use('/api/v1/reviews/', reviewRouter);
 app.use('/api/v1/orders/', orderRouter);
-
-//Stripe webhook
-//stripe instance
-const stripe = new Stripe(process.env.STRIPE_KEY);
-
-
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = "whsec_bcb4cd56075994cfc386e015d9fcd8e511e5f5177aacb9ab18a1c3fac59effba";
-
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
-
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    console.log("event");
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntentSucceeded = event.data.object;
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
-
 
 //err middleware
 app.use(notFound);
