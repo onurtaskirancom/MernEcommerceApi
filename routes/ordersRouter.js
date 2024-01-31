16a42667c9bf2198fa217718aff9f36ea1b85091
onurtaskirancom <taskiranonur9@gmail.com>
2024-01-31 15:21:15 +0300

    Admin Processing Order

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -3,6 +3,7 @@ import {
   createOrderCtrl,
   getAllordersCtrl,
   getSingleOrderCtrl,
+  updateOrderCtrl,
 } from '../controllers/orderCtrl.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
@@ -10,6 +11,7 @@ const orderRouter = exppress.Router();
 
 orderRouter.post('/', isLoggedIn, createOrderCtrl);
 orderRouter.get('/', isLoggedIn, getAllordersCtrl);
+orderRouter.put('/update/:id', isLoggedIn, updateOrderCtrl);
 orderRouter.get('/:id', isLoggedIn, getSingleOrderCtrl);
 
 
