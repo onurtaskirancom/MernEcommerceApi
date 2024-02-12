75fa5683b35ee1475d53acd7a6489d990c855e36
onurtaskirancom <taskiranonur9@gmail.com>
2024-02-12 16:56:00 +0300

    Create Product With Images

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -8,59 +8,60 @@ import Category from '../model/Category.js';
 // @access  Private/Admin
 
 export const createProductCtrl = asyncHandler(async (req, res) => {
-  console.log(req.files);
-  // const { name, description, category, sizes, colors, price, totalQty, brand } =
-  //   req.body;
-  // //Product exists
-  // const productExists = await Product.findOne({ name });
-  // if (productExists) {
-  //   throw new Error('Product Already Exists');
-  // }
-  // //find the brand
-  // const brandFound = await Brand.findOne({
-  //   name: brand?.toLowerCase(),
-  // });
+  const convertedImgs = req.files.map((file) => file?.path);
+  const { name, description, category, sizes, colors, price, totalQty, brand } =
+    req.body;
+  //Product exists
+  const productExists = await Product.findOne({ name });
+  if (productExists) {
+    throw new Error('Product Already Exists');
+  }
+  //find the brand
+  const brandFound = await Brand.findOne({
+    name: brand?.toLowerCase(),
+  });
 
-  // if (!brandFound) {
-  //   throw new Error(
-  //     'Brand not found, please create brand first or check brand name'
-  //   );
-  // }
-  // //find the category
-  // const categoryFound = await Category.findOne({
-  //   name: category,
-  // });
-  // if (!categoryFound) {
-  //   throw new Error(
-  //     'Category not found, please create category first or check category name'
-  //   );
-  // }
-  // //create the product
-  // const product = await Product.create({
-  //   name,
-  //   description,
-  //   category,
-  //   sizes,
-  //   colors,
-  //   user: req.userAuthId,
-  //   price,
-  //   totalQty,
-  //   brand,
-  // });
-  // //push the product into category
-  // categoryFound.products.push(product._id);
-  // //resave
-  // await categoryFound.save();
-  // //push the product into brand
-  // brandFound.products.push(product._id);
-  // //resave
-  // await brandFound.save();
-  // //send response
-  // res.json({
-  //   status: 'success',
-  //   message: 'Product created successfully',
-  //   product,
-  // });
+  if (!brandFound) {
+    throw new Error(
+      'Brand not found, please create brand first or check brand name'
+    );
+  }
+  //find the category
+  const categoryFound = await Category.findOne({
+    name: category,
+  });
+  if (!categoryFound) {
+    throw new Error(
+      'Category not found, please create category first or check category name'
+    );
+  }
+  //create the product
+  const product = await Product.create({
+    name,
+    description,
+    category,
+    sizes,
+    colors,
+    user: req.userAuthId,
+    price,
+    totalQty,
+    brand,
+    images: convertedImgs,
+  });
+  //push the product into category
+  categoryFound.products.push(product._id);
+  //resave
+  await categoryFound.save();
+  //push the product into brand
+  brandFound.products.push(product._id);
+  //resave
+  await brandFound.save();
+  //send response
+  res.json({
+    status: 'success',
+    message: 'Product created successfully',
+    product,
+  });
 });
 
 // @desc    Get all products
