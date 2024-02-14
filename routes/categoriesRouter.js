0728cfb133e3c73722a9113704d43642a3dbbe93
onurtaskirancom <taskiranonur9@gmail.com>
2024-02-14 16:28:35 +0300

    Create Category With Image

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,4 +1,5 @@
 import exppress from 'express';
+import catetgoryFileUpload from '../config/categoryUpload.js';
 
 import {
   createCategoryCtrl,
@@ -11,10 +12,15 @@ import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
 const categoriesRouter = exppress.Router();
 
-categoriesRouter.post('/', isLoggedIn, createCategoryCtrl);
+categoriesRouter.post(
+  '/',
+  isLoggedIn,
+  catetgoryFileUpload.single('file'),
+  createCategoryCtrl
+);
 categoriesRouter.get('/', getAllCategoriesCtrl);
 categoriesRouter.get('/:id', getSingleCategoryCtrl);
 categoriesRouter.delete('/:id', deleteCategoryCtrl);
-categoriesRouter.put('/:id', updateCategoryCtrl); 
+categoriesRouter.put('/:id', updateCategoryCtrl);
 
 export default categoriesRouter;
